60e3e3efba4415087b775997ca2f212904d54432
ramos-emily <ramos.emilyvi@gmail.com>
2025-04-04 11:08:52 -0300

    task 1

--- livros/views.py
+++ livros/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from .models import Livro
+from rest_framework.decorators import api_view
 
 
 #listar
+@api_view(['GET', 'POST'])
 def listar_livro(request):
-    livros = Livro.objects.all()
-    return render(request, 'templates/livros.html', {'livros': livros})
-
+    if request.method == "GET":
+        livros = Livro.objects.all()
+        serializer = LivroSerializer(livros, many=True)
+        return render(request, 'livros.html', {'livros': livros})
+    elif request.method == "POST":
+        serializer = LivroSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
